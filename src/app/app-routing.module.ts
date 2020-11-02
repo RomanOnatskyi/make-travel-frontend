e98f3761ec8b3f8e059391cf592e1846783d7162
Roman Onatskyi <roman.onatskyi@gmail.com>
2020-11-02 10:09:56 +0200

    Create main page template. Update routs.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './ui/pages/main/main/main.component';
 import { SignUpComponent } from './ui/pages/auth/components/sign-up.component';
 import { SignInComponent } from './ui/pages/auth/components/sign-in.component';
 
 const routes: Routes = [
-    // { path: '', component: Main },
+    { path: '', redirectTo: '/main', pathMatch: 'full' },
+    { path: 'main', component: MainComponent },
     { path: 'auth/sign-up', component: SignUpComponent },
     { path: 'auth/sign-in', component: SignInComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderModule } from './ui/header/header.module';
 import { AppRoutingModule } from './app-routing.module';
-import { AuthModule } from './ui/pages/auth/auth.module';
+import { HeaderModule } from './ui/header/header.module';
 import { FooterModule } from './ui/footer/footer.module';
+import { MainModule } from './ui/pages/main/main.module';
+import { AuthModule } from './ui/pages/auth/auth.module';
 
 @NgModule({
     declarations: [
@@ -15,8 +16,9 @@ import { FooterModule } from './ui/footer/footer.module';
         BrowserModule,
         AppRoutingModule,
         HeaderModule,
-        AuthModule,
         FooterModule,
+        MainModule,
+        AuthModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
