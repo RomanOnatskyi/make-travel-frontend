732121c093f1b359b89530a1495e70a4c5b0d251
Roman Onatskyi <roman.onatskyi@gmail.com>
2020-12-04 16:23:28 +0200

    complete Service-creation page

--- src/app/ui/pages/create-service/create-service/create-service.component.ts
+++ src/app/ui/pages/create-service/create-service/create-service.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppStateService, ServiceCategory } from '../../../../app-state.service';
+import { NewHotelService } from '../new-hotel-service';
+import { CreateServiceService } from '../create-service.service';
+import { BaseResponse } from '../../../../responses/base-response';
 
 @Component({
     selector: 'app-create-service',
@@ -7,10 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateServiceComponent implements OnInit {
 
-    constructor() {
+    constructor(
+        private router: Router,
+        private appStateService: AppStateService,
+        private createServiceService: CreateServiceService,
+    ) {}
+
+    ngOnInit() {
+
+        this.newHotelService.category = ServiceCategory.Cleaning;
     }
 
-    ngOnInit(): void {
+    categories = [
+        { name: "Уборка", value: ServiceCategory.Cleaning },
+        { name: "Еда", value: ServiceCategory.Food },
+        { name: "Развлечения", value: ServiceCategory.Entertainment },
+        { name: "Ремонт", value: ServiceCategory.Repairing },
+    ];
+
+    newHotelService = new NewHotelService();
+    processing: boolean = false;
+    serviceCreationError: string = null;
+
+    submit() {
+
+        this.processing = true;
+
+        this.createServiceService.createService(this.newHotelService)
+            .subscribe(response => this.handleResponse(response));
+    }
+
+    private handleResponse(response: BaseResponse) {
+
+        this.processing = false;
+        this.serviceCreationError = response.errors;
+
+        if (this.serviceCreationError) {
+            window.scrollTo(0, 0);
+            return;
+        }
+
+        alert('Услуга успешно создана!');
     }
 
 }
---
+++ src/app/ui/pages/create-service/new-hotel-service.ts
@@ -0,0 +1,8 @@
+import { ServiceCategory } from '../../../app-state.service';
+
+export class NewHotelService {
+    name: string;
+    description: string;
+    category: ServiceCategory;
+    price: number;
+}
