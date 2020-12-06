cc419a301935efcbeb07d595cb988c191c8715f6
Roman Onatskyi <roman.onatskyi@gmail.com>
2020-12-06 09:25:22 +0200

    add hotel-orders service and appropriate methods, configure ngFor

--- src/app/ui/pages/hotel-orders/hotel-orders/hotel-orders.component.ts
+++ src/app/ui/pages/hotel-orders/hotel-orders/hotel-orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HotelOrdersService } from '../hotel-orders.service';
+import { HotelOrder } from '../../../../responses/hotel-orders-response';
 
 @Component({
     selector: 'app-hotel-orders',
@@ -7,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HotelOrdersComponent implements OnInit {
 
-    constructor() {
-    }
+    constructor(
+        private hotelOrdersService: HotelOrdersService,
+    ) {}
 
     ngOnInit(): void {
+
+        this.hotelOrders = this.getHotelOrders();
     }
 
+    hotelOrders: HotelOrder[];
+
+    private getHotelOrders() {
+
+        let orders = [];
+
+        this.hotelOrdersService.getHotelOrders()
+            .subscribe(response => {
+
+                this.showError([response.errors]);
+
+                orders = response.hotelList;
+            });
+
+        return orders;
+    }
+
+    private showError(errors: string[]) {
+
+        for (let i = 0; i < errors.length; i++) {
+            if (errors[i]) {
+                alert(errors[i]);
+            }
+        }
+    }
 }
