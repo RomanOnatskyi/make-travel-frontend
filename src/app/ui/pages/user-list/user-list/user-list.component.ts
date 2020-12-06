c2193c3a1c9efea60ea4ca0444910b96d4c16988
Roman Onatskyi <roman.onatskyi@gmail.com>
2020-12-06 08:47:34 +0200

    add user-list service and appropriate methods, configure ngFor

--- src/app/ui/pages/user-list/user-list/user-list.component.ts
+++ src/app/ui/pages/user-list/user-list/user-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserListService } from '../user-list.service';
+import { User } from '../../../../responses/user-list-response';
 
 @Component({
     selector: 'app-user-list',
@@ -7,10 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-    constructor() {
+    constructor(
+        private userListService: UserListService,
+    ) {}
+
+    async ngOnInit() {
+
+        this.users = await this.getAllUsers();
     }
 
-    ngOnInit(): void {
+    users: User[];
+
+    private async getAllUsers() {
+
+        const bannedUsersResponse = await this.userListService.getBannedUsers().toPromise();
+        const commonUsersResponse = await this.userListService.getCommonUsers().toPromise();
+
+        const error = [ bannedUsersResponse.errors, commonUsersResponse.errors ];
+
+        this.showError(error);
+
+        let resultArrayOfUsers = bannedUsersResponse.userList;
+
+        Array.prototype.push.apply(resultArrayOfUsers, commonUsersResponse.userList);
+
+        return resultArrayOfUsers;
     }
 
+    async banUser(userId: number) {
+
+        this.userListService.banUser(userId)
+            .subscribe(response => this.showError([ response.errors ]));
+
+        this.users = await this.getAllUsers();
+    }
+
+    async unbanUser(userId: number) {
+
+        this.userListService.unbanUser(userId)
+            .subscribe(response => this.showError([ response.errors ]));
+
+        this.users = await this.getAllUsers();
+    }
+
+    private showError(errors: string[]) {
+
+        for (let i = 0; i < errors.length; i++) {
+            if (errors[i]) {
+                alert(errors[i]);
+            }
+        }
+    }
 }
