ec8283edb49824236c10f4edbb62a5299abd50b5
Roman Onatskyi <roman.onatskyi@gmail.com>
2020-12-24 19:58:37 +0200

    create my-hotels page template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ServicesModule } from './ui/pages/services/services.module';
 import { CreateServiceModule } from './ui/pages/create-service/create-service.module';
 import { HotelOrdersModule } from './ui/pages/hotel-orders/hotel-orders.module';
 import { UserListModule } from './ui/pages/user-list/user-list.module';
+import { MyHotelsModule } from './ui/pages/my-hotels/my-hotels.module';
 
 @NgModule({
     declarations: [
@@ -29,6 +30,7 @@ import { UserListModule } from './ui/pages/user-list/user-list.module';
         CreateServiceModule,
         HotelOrdersModule,
         UserListModule,
+        MyHotelsModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
