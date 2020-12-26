6bfc1739b182a3c35ca7239bc9f58b76d78dfa18
Roman Onatskyi <roman.onatskyi@gmail.com>
2020-12-26 22:41:27 +0200

    create my-hotels service

--- src/app/ui/pages/my-hotels/my-hotels/my-hotels.component.ts
+++ src/app/ui/pages/my-hotels/my-hotels/my-hotels.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { UserHotel } from '../../../../responses/user-hotel-response';
+import { Feedback } from '../feedback';
+import { MyHotelsService } from '../my-hotels';
 
 @Component({
-  selector: 'app-my-hotels',
-  templateUrl: './my-hotels.component.html',
-  styleUrls: ['./my-hotels.component.css']
+    selector: 'app-my-hotels',
+    templateUrl: './my-hotels.component.html',
+    styleUrls: ['./my-hotels.component.css'],
 })
 export class MyHotelsComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private myHotelsService: MyHotelsService,
+    ) {}
 
-  ngOnInit(): void {
-  }
+    userHotels: UserHotel[];
+    feedback: Feedback;
 
+    async ngOnInit() {
+
+        this.userHotels = await this.getUserHotels();
+    }
+
+    private async getUserHotels() {
+
+        const userHotelsResponse = await this.myHotelsService.getUserHotels().toPromise();
+
+        const error = [ userHotelsResponse.errors ];
+
+        this.showError(error);
+
+        return userHotelsResponse.hotelList;
+    }
+
+    private leaveFeedback(hotelId: number) {
+
+        // todo связать форму с объектом фидбэка
+
+        this.feedback.hotelId = hotelId;
+
+        this.myHotelsService.sendFeedback(this.feedback)
+            .subscribe(response => this.showError([ response.errors ]));
+
+        alert("Ваш отзыв принят");
+    }
+
+    private showError(errors: string[]) {
+
+        for (let i = 0; i < errors.length; i++) {
+            if (errors[i]) {
+                alert(errors[i]);
+            }
+        }
+    }
 }
